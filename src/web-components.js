bfb95406b3f27de0c793697961fd2b9a63d834ca
jjdcabasolo <abasolojohnjourish@gmail.com>
2020-04-23 09:22:55 +0800

    Fixed import statements

--- src/web-components.js
+++ src/web-components.js
@@ -1,21 +1,20 @@
 // import { WebComponents } from './WebComponents.js';
 // customElements.define('web-components', WebComponents);
 
-/* eslint-disable no-unused-vars */
 // https://webcomponents.dev/edit/collection/hzgpcWmPGvSJXSo4fs2a
-import { BasicSetup } from './lit-basic/01-basic-setup/BasicSetup.js';
-import { ManageProperties } from './lit-basic/02-manage-properties/ManageProperties.js';
-import { PropertyChanges } from './lit-basic/03-property-changes/PropertyChanges.js';
-import { PropertiesAndAttributes } from './lit-basic/04-properties-and-attributes/PropertiesAndAttributes.js';
-import { FancyMessage } from './lit-basic/04-properties-and-attributes/FancyMessage.js';
-import { PassingProperties } from './lit-basic/05-passing-properties/PassingProperties.js';
-import { AuthorProfile } from './lit-basic/05-passing-properties/AuthorProfile.js';
-import { HandleEvents } from './lit-basic/06-handle-events/HandleEvents.js';
-import { ConditionalRendering } from './lit-basic/07-conditional-rendering/ConditionalRendering.js';
-import { RepeatedTemplates } from './lit-basic/08-repeated-templates/RepeatedTemplates.js';
-import { UpdateArraysAndObjects } from './lit-basic/09-update-arrays-and-objects/UpdateArraysAndObjects.js';
-import { RenderStyles } from './lit-basic/10-render-styles/RenderStyles.js';
-import { FetchingData } from './lit-basic/11-fetching-data/FetchingData.js';
-import { FireEventsParent } from './lit-basic/12-firing-events/FireEventsParent.js';
-import { FireEventsChild } from './lit-basic/12-firing-events/FireEventsChild.js';
-import { SectionHeader } from './lit-basic/SectionHeader.js';
+import './lit-basic/01-basic-setup/BasicSetup.js';
+import './lit-basic/02-manage-properties/ManageProperties.js';
+import './lit-basic/03-property-changes/PropertyChanges.js';
+import './lit-basic/04-properties-and-attributes/PropertiesAndAttributes.js';
+import './lit-basic/04-properties-and-attributes/FancyMessage.js';
+import './lit-basic/05-passing-properties/PassingProperties.js';
+import './lit-basic/05-passing-properties/AuthorProfile.js';
+import './lit-basic/06-handle-events/HandleEvents.js';
+import './lit-basic/07-conditional-rendering/ConditionalRendering.js';
+import './lit-basic/08-repeated-templates/RepeatedTemplates.js';
+import './lit-basic/09-update-arrays-and-objects/UpdateArraysAndObjects.js';
+import './lit-basic/10-render-styles/RenderStyles.js';
+import './lit-basic/11-fetching-data/FetchingData.js';
+import './lit-basic/12-firing-events/FireEventsParent.js';
+import './lit-basic/12-firing-events/FireEventsChild.js';
+import './lit-basic/SectionHeader.js';
